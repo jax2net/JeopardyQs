3ea4159b793f1c89591a53c3f1573a741cd17025
Jacob Santos <jsns@disroot.org>
2020-05-22 00:07:34 -0700

    Updates

---
+++ precache-manifest.328eea7916e43a83b5309e9027743ad1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0615733a9a9fe6a23d45bdd7be230ad",
+    "url": "/JeopardyQs/index.html"
+  },
+  {
+    "revision": "ac36198d0efbc663f956",
+    "url": "/JeopardyQs/static/css/main.16b63eec.chunk.css"
+  },
+  {
+    "revision": "254379f07da08ee44c92",
+    "url": "/JeopardyQs/static/js/2.c61d151f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JeopardyQs/static/js/2.c61d151f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac36198d0efbc663f956",
+    "url": "/JeopardyQs/static/js/main.60f4b67f.chunk.js"
+  },
+  {
+    "revision": "d080337425efb1f16ed9",
+    "url": "/JeopardyQs/static/js/runtime-main.30d767a9.js"
+  }
+]);
