4dfb56b6e5b73e8d0265e010812b3b3faa802739
jsns <jsns@jake>
2020-05-22 00:02:38 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    state = { difficulty: "", question: "", answer: "", title: "", display: "", hasBeenClicked: false };
+
+    componentDidMount() {
+        this.getQ();
+    }
+
+    getQ = () => {
+        axios
+            .get("https://jservice.io/api/random")
+            .then((response) => {
+                console.log(response);
+                const { answer, value, question } = response.data[0];
+                const { title } = response.data[0].category;
+                this.setState({
+                    difficulty: value,
+                    question: question,
+                    answer: answer,
+                    title: title,
+                    hasBeenClicked: false
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
+    handleClick = () => {
+        this.setState(prevState => ({
+            hasBeenClicked: !prevState.hasBeenClicked 
+        }));
+    }
+
+    render() {
+        return (
+<React.Fragment>
+			<h1>Random Jeopardy Question</h1>
+			<h3>Click inside the blue to see the answer</h3>
+            <div className="app">
+            <div className="card" onClick={this.handleClick}>
+            <div id='desc'>In  <span id='categoryText'><i>{this.state.title}</i></span>   for   <span id='moneyText'>{this.state.difficulty}</span>... </div>
+            <h2 className="heading">{this.state.hasBeenClicked ? this.state.answer : this.state.question}</h2>
+            <button className="hvr-grow" onClick={this.getQ}>
+            Next Question
+            </button>
+            </div>
+            </div>
+</React.Fragment>
+        );
+    }
 }
 
 export default App;
